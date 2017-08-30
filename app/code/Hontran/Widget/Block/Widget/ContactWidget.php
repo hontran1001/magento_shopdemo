b9fa197f8f5aa77111be265d9b40d70a3f804e67
hontran <hontran1001@gmail.com>
2017-08-31 02:51:51 +0700

    widget

---
+++ app/code/Hontran/Widget/Block/Widget/ContactWidget.php
@@ -0,0 +1,10 @@
+<?php
+namespace Hontran\Widget\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class ContactWidget extends Template implements BlockInterface
+{
+
+}
